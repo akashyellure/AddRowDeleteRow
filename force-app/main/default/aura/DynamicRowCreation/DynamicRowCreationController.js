eb975269def81b6b9cd05f1a9db6fb11515a927c
akash yellure <yellure9707@gmail.com>
2022-11-21 18:43:39 +0530

    Updated

--- force-app/main/default/aura/DynamicRowCreation/DynamicRowCreationController.js
+++ force-app/main/default/aura/DynamicRowCreation/DynamicRowCreationController.js
@@ -1,5 +1,64 @@
 ({
-    myAction : function(component, event, helper) {
-
-    }
+    doInit : function(component, event, helper) {
+        helper.addRowHelper(component, 1);
+    },
+    
+    addRow : function(component, event, helper) {
+        var loopVar = 1;
+        if(event.getSource().get('v.name') === 'multipleRows' && !$A.util.isEmpty('v.numberOfRows') ) {
+            loopVar = component.get('v.numberOfRows') > 15 ? 0 : component.get('v.numberOfRows');
+        }
+        helper.addRowHelper(component, loopVar);
+    },
+    
+    editRow : function(component, event, helper) {
+        var dynamicRowsList = component.get("v.dynamicRowsList");
+        var index = dynamicRowsList.findIndex(x => x.rowNumber === event.getSource().get('v.name'))
+        if(index != -1)
+            dynamicRowsList[index].editMode = !dynamicRowsList[index].editMode;
+        component.set("v.dynamicRowsList", dynamicRowsList);
+    },
+    
+    deleteRow : function(component, event, helper) {
+        var dynamicRowsList = component.get("v.dynamicRowsList");
+        var index = dynamicRowsList.findIndex(x => x.rowNumber === event.getSource().get('v.name'))
+        if(index != -1)
+            dynamicRowsList.splice(index, 1);
+        component.set("v.dynamicRowsList", dynamicRowsList);
+    },
+    
+    cloneRow : function(component, event, helper) {
+        var dynamicRowsList = component.get("v.dynamicRowsList");
+        var index = dynamicRowsList.findIndex(x => x.rowNumber === event.getSource().get('v.name'))
+        if(index != -1) {
+            dynamicRowsList.push({
+                'Name': dynamicRowsList[index].Name,
+                'Phone': dynamicRowsList[index].Phone,
+                'AccountNumber': dynamicRowsList[index].AccountNumber,
+                'editMode': dynamicRowsList[index].editMode,
+                'rowNumber':dynamicRowsList.length + 1
+            });
+        }
+        component.set("v.dynamicRowsList", dynamicRowsList);
+    },
+    
+    incrementRow: function(component, event, helper) {
+        var numberOfRows = component.get("v.numberOfRows");
+        component.set("v.numberOfRows", numberOfRows + 1);
+    },
+    
+    decrementRow: function(component, event, helper) {
+        var numberOfRows = component.get("v.numberOfRows");
+        component.set("v.numberOfRows", numberOfRows - 1);
+    },
+    
+    saveRecords : function(component, event, helper) {
+        var validExpense = component.find('dynamiRowsForm').reduce(function ( validSoFar, inputCmp ) {
+            inputCmp.showHelpMessageIfInvalid();
+            return validSoFar && inputCmp.get('v.validity').valid;
+        }, true);
+        if(validExpense){
+            helper.saveRecordsHelper(component, event, helper);
+        }
+    },
 })
--- force-app/main/default/aura/DynamicRowCreation/DynamicRowCreationHelper.js
+++ force-app/main/default/aura/DynamicRowCreation/DynamicRowCreationHelper.js
@@ -1,5 +1,50 @@
 ({
-    helperMethod : function() {
-
-    }
+    addRowHelper : function(component, noOfRows) {
+        for(var index = 0; index < noOfRows; index++) {
+var dynamicRowsList = component.get("v.dynamicRowsList");
+        dynamicRowsList.push({
+            'Name': '',
+            'Phone': '',
+            'AccountNumber': '',
+            'editMode': true,
+            'rowNumber':dynamicRowsList.length + 1
+        });
+        component.set("v.dynamicRowsList", dynamicRowsList);
+}
+    },
+    
+    saveRecordsHelper : function(component, event, helper) {
+var action = component.get('c.saveAccountRecords');
+        action.setParams({
+            'dynamicRowsList' : JSON.stringify(component.get('v.dynamicRowsList'))
+        });
+        action.setCallback(this,function(response){
+         var result = response.getReturnValue();
+            if(response.getState() === 'SUCCESS') {
+                if(result.messageType === 'success') {
+                    var dynamicRowList = [];
+                    dynamicRowList.push({
+                        'Name': '',
+                        'Phone': '',
+                        'AccountNumber': '',
+                        'editMode': true,
+                        'rowNumber':dynamicRowList.length + 1
+                    });  
+                    component.set("v.dynamicRowsList", dynamicRowList);
+                    component.set('v.messageType','success');
+                    component.set('v.message','Records created succesfully');
+                } else {
+                    component.set('v.messageType','error');
+                    component.set('v.message',result.message);
+                }
+            } else {
+                component.set('v.messageType','error');
+                component.set('v.message','No response from server or client is offline.');
+            }
+        });
+        $A.enqueueAction(action);
+        setTimeout($A.getCallback(function() {
+                component.set('v.message','');
+        }), 5000);
+},
 })
